445112ca7272e864fcedf4f7659321525cac93f0
Aurora Shi <aurorashi16@gmail.com>
2024-01-31 10:35:12 -0500

    icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,10 @@ import React from 'react'
 import './Footer.css'
 import {Link} from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faGithub} from '@fortawesome/free-brands-svg-icons'
-
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
     return (
@@ -13,16 +15,19 @@ function Footer() {
                   <small className='website-rights'>Aurora Shi © 2024</small>
                   <div className="social-icons">
                       <a className="social-icon-link email" href = "mailto: Aurorashi16@gmail.com">
-                          <i className="far fa-envelope"></i>
+                      <FontAwesomeIcon icon={faEnvelope} />
+
                       </a>
                       <Link className="social-icon-link instagram" to="https://www.instagram.com/aurorashi16/" target="_blank">
-                          <i className="fab fa-instagram"></i>
+                      <FontAwesomeIcon icon={faInstagram} />
+
                       </Link>
                       <Link className="social-icon-link github" to="https://github.com/asyf16" target="_blank">
                         <FontAwesomeIcon icon={faGithub} />
                       </Link>
                       <Link className="social-icon-link linkedin" to="https://www.linkedin.com/in/aurora-shi-387656247/" target="_blank">
-                          <i className="fab fa-linkedin"></i>
+                      <FontAwesomeIcon icon={faLinkedin} />
+
                       </Link>
                   </div>
               </div>
